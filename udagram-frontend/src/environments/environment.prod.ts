7025fd959a4b6eb94005f1738174058b3c27e568
Dany Díaz <dany_diaz_22@hotmail.com>
2021-01-10 12:34:41 -0600

    feat: Make reverse proxy name configurable with environment variables from front end site.

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: process.env.REVERSEPROXYNAME ? `http://${process.env.REVERSEPROXYNAME}:8080/api/v0` : 'http://localhost:8080/api/v0'
 };
 
 /*
